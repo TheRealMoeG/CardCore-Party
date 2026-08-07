3bb7289de6a792d6fae64b5f5777c06f9cbe90ba
TheRealMoeG <mrblackburn93@gmail.com>
2026-08-07 17:21:21 -0400

    Delete cardcore-party-v0.1 directory

--- cardcore-party-v0.1/src/main/java/com/cardcoreparty/CardCorePartyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.cardcoreparty;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-
-@ConfigGroup("cardcoreparty")
-public interface CardCorePartyConfig extends Config
-{
-}
