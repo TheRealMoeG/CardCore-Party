5832048a1da4c433e6be9b8fc9e95108cb46dc82
TheRealMoeG <mrblackburn93@gmail.com>
2026-08-07 17:22:53 -0400

    Add files via upload

---
+++ src/main/java/com/cardcoreparty/CardCorePartyConfig.java
@@ -0,0 +1,9 @@
+package com.cardcoreparty;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("cardcoreparty")
+public interface CardCorePartyConfig extends Config
+{
+}
---
+++ src/main/java/com/cardcoreparty/party/CardCoreCollectionMessage.java
@@ -0,0 +1,15 @@
+package com.cardcoreparty.party;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import net.runelite.client.party.messages.PartyMemberMessage;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class CardCoreCollectionMessage extends PartyMemberMessage
+{
+    private long revision;
+    private List<String> ownedNames = new ArrayList<>();
+}
